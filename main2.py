eae55c923128b67b5c95dd1bd112fa222216dfb6
fallamjingasana <championjeremy26@gmail.com>
2026-03-03 20:01:46 +0300

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,26 +1,12 @@
-import random
-while True:
-    user_action = input("enter a choice (rock.paper,scissors)")
-    possible_action=["rock, paper,scissors"]
-    computer_action= random.choice(possible_action)
-    print(f"You chose {user_action},computer chose{computer_action}")
-    if user_action== computer_action:
-        print("it is a tie")
-    elif user_action=="rock":
-        if computer_action=="scissors":
-            print("you win")
-        else:
-         print("you lose")
-    elif user_action=="paper":
-        if computer_action=="rock":
-            print("you win")
-        else:
-         print("you lose")
-    elif user_action=="scissors":
-        if computer_action=="paper":
-            print("you win")
-        else:
-         print("you lose")
-    play= input("play again ?")
-    if play != "y":
-        break
+def match_words(words):
+    ctr = 0
+    lst = []
+
+    for word in words:
+      if len(word)>1 and word[0]==word[-1]:
+        ctr+= 1
+        lst.append(word)
+    print(lst)
+    return ctr
+count = match_words(["abc","ete","gfd","gad"])
+print(count)
--- main3.py
+++ main3.py
@@ -1,6 +1,11 @@
-import math
-print(math.ceil(23.56))
-x = 10
-y =-5
-print(str(math.copysign(x,y)))
-print(math.fabs(-96))
+l = [2,5,6,3,9,4]
+print("print original list:",l)
+count = 0
+for i in l:
+    count+=i
+avg = count/len(l)
+print("sum =",count)
+print("average",avg)
+l.sort()
+print(l[0])
+print(l[-1])
