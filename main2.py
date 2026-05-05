d0bbe8a609609e068cbcf59f8ffac095a94fcb37
fallamjingasana <championjeremy26@gmail.com>
2026-05-05 19:49:13 +0300

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,14 +1,31 @@
-import pygame
-pygame.init()
-window = pygame.display.set_mode((400,400))
-window.fill((255,255,255))
-GREEN = (0,255,0)
-pygame.draw.circle(window,GREEN,(300,300),50)
-pygame.draw.circle(window,GREEN,(100,100),50,3)
-pygame.display.update()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type ==pygame.QUIT:
-            running = False
-pygame.quit()
+from tkinter import *
+from datetime import date
+root = Tk()
+
+root.title('grtting started with widgets')
+root.geometry('400x200')
+
+lbl = Label(text = 'hey theyre', fg ='white',bg ='blue',height=1,width=300,)
+
+name_lbl = Label(text='full name',bg='blue')
+name_entry =Entry()
+
+def display():
+    name= name_entry.get()
+    global Message
+    Message = 'welcom to the aplication \n todays date is :'
+    greet = 'hello' + name +'\n'
+
+    text_box.insert(EMD,greet)
+    text_box.insert(EMD,message)
+    text_box.insert(EMD,today())
+text_box = Text(height=5)
+
+btn = Button(text='begin', command = display,height=1,fg='red',bg='cyan')
+
+lbl.pack()
+name_lbl.pack()
+name_entry.pack()
+btn.pack()
+text_box.pack()
+root.mainloop()
